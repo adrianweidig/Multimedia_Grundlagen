a6b103a58ba5ea90224ec8bd9bb4879ab7cd63b8
Adrian Weidig <70036819+adrianweidig@users.noreply.github.com>
2024-07-05 23:39:29 +0200

    Update NavBar.vue

--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,8 +1,8 @@
 <template>
-  <nav>
-    <router-link to="/aufgabe1">Aufgabe 1</router-link> |
-    <router-link to="/aufgabe2">Aufgabe 2</router-link> |
-    <router-link to="/aufgabe3">Aufgabe 3</router-link>
+  <nav class="navbar">
+    <router-link to="/aufgabe1" class="nav-link">Aufgabe 1</router-link>
+    <router-link to="/aufgabe2" class="nav-link">Aufgabe 2</router-link>
+    <router-link to="/aufgabe3" class="nav-link">Aufgabe 3</router-link>
   </nav>
 </template>
 
